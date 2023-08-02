cd5bfdb2cded58ce6afecdddd8911ba844f79390
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-08-02 17:33:24 +0100

    make requested changes

--- rental.rb
+++ rental.rb
@@ -1,9 +1,13 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
-    @date = date
+  def initialize(_date, book, person)
     @book = book
+
+    book.rentals << self
+
     @person = person
+
+    person.rentals << self
   end
 end
