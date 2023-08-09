af814a610ff9da7ce53719b765033a095b7cca41
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-08-09 12:28:45 +0100

    linter error corrected

--- app/app.rb
+++ app/app.rb
@@ -47,4 +47,3 @@ class App
     @filemanager.create
   end
 end
-
--- data-managers/bookwriter.rb
+++ data-managers/bookwriter.rb
@@ -1,14 +1,14 @@
 require 'json'
 
 class BookWriter
- def initialize(book_list)
+  def initialize(book_list)
     @book_list = book_list
     @file_path = '../data/books.json'
   end
- 
+
   def read
     return [] unless File.exist?(@file_path) && !File.empty?(@file_path)
- 
+
     books = []
     File.open(@file_path, 'r') do |file|
       JSON.parse(file.read).each do |data|
@@ -18,7 +18,7 @@ class BookWriter
     end
     books
   end
- 
+
   def create
     File.write(@file_path, JSON.generate(@books_list.map(&:as_json)))
   end
